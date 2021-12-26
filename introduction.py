03c8f72923a4e163891a75a2cb943f81c5fb889c
rubenbaduwal <baduwalramesh98@gmail.com>
2021-12-26 16:42:30 +0545

    introduction py

---
+++ introduction.py
@@ -0,0 +1,23 @@
+print('i am  very happy!')
+# it is used for comment
+# arithmetic operator +,-,/,*,%,//(floor division),**(power)
+
+print(3/4)
+print(4//2)
+print(5 % 2)
+print(2**5)
+print(6+4)
+print(8-3)
+print(5-4+14/2*10 % 3)
+
+
+# variable -> to store value
+#eg: a=25
+
+x = 20
+print(x)
+
+
+a=21
+b=11
+print(a-b)
